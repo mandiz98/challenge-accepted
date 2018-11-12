aa171e72cde8a12704c0db32979b78fd776fb19e
Jacob Carlquist <caja1694@mace2428-08.jth-stud1.hj.se>
2018-11-12 17:02:14 +0100

    WIP

--- Challenge Accepted/View Controller/ProfileViewController.swift
+++ Challenge Accepted/View Controller/ProfileViewController.swift
@@ -7,15 +7,54 @@
 //
 
 import UIKit
+import FacebookCore
+import FBSDKCoreKit
+
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var ProfilePicture: UIImageView!
+    @IBOutlet weak var ProfileName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getFacebookName()
+        getFacebookPic()
         // Do any additional setup after loading the view.
     }
     
+    func getFacebookName(){
+        let parameters = ["fields": "first_name, last_name"]
+        FBSDKGraphRequest(graphPath: "/me", parameters: parameters).start{
+            
+            (connection, result, err) in
+            if err != nil {
+                print(err!)
+                return
+            }
+            let data:[String:Any] = result as! [String:Any]
+            self.ProfileName.text = "\(data["first_name"] as! String) \(data["last_name"] as! String)"
+        }
+    }
+    func getFacebookPic(){
+        let parameters = ["fields": "picture.width(512).height(512)"]
+        FBSDKGraphRequest(graphPath: "/me", parameters: parameters).start{
+            (connection, result, err) in
+            if err != nil{
+                print(err!)
+                return
+            }
+            let field = result! as? [String:Any]
+            let imageUrl = ((field!["picture"] as? [String: Any])?["data"] as? [String: Any])?["url"] as? String
+            print("Min URL:", imageUrl!)
+            let data = NSData(contentsOf: URL(string: imageUrl!)!)
+            let image = UIImage(data: data! as Data)
+            self.ProfilePicture.image = image
+        }
 
+    }
+    
     /*
     // MARK: - Navigation
 
