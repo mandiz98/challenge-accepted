967fa86face76d582b36508ff2915b17cda5b74c
Erik Andreasson <aner1797@mace2428-10.jth-stud1.hj.se>
2018-11-13 14:16:28 +0100

    data

--- Challenge Accepted/RoundImageView.swift
+++ Challenge Accepted/RoundImageView.swift
@@ -1,9 +1,9 @@
 //
 //  RoundImageView.swift
-//  Challenge Accepted
+//  MVCDemo
 //
-//  Created by Amanda Axelsson on 2018-11-06.
-//  Copyright © 2018 Amanda Axelsson. All rights reserved.
+//  Created by Ebbas on 2018-11-01.
+//  Copyright © 2018 enappstudio. All rights reserved.
 //
 
 import UIKit
@@ -11,9 +11,31 @@ import UIKit
 @IBDesignable
 class RoundImageView: UIImageView {
     
+    @IBInspectable
+    public var borderColor: UIColor = UIColor.red {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    public var borderWidth: CGFloat = 1.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+            
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
     override func layoutSubviews() {
         self.layer.cornerRadius = self.frame.size.height / 2
         self.clipsToBounds = true
     }
-    
 }
